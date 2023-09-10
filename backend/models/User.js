4a58546ff444ad8d43521206a14972e7915b2281
Shenal Tissera <93712524+shenaltissera@users.noreply.github.com>
2023-09-10 21:25:01 +0530

    changing the file directory

---
+++ backend/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
+    photo: {
+      type: String,
+    },
+
+    role: {
+      type: String,
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
